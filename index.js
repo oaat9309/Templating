5053e46015e2e27b3a48e6344aba26f7de1e4ef9
oaat9309 <kite1993@naver.com>
2021-12-07 23:23:36 +0900

    Initial commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require("express")
+const app = express();
+const path = require('path');
+const redditData = require('./data.json')
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, "/views"))
+
+app.get("/", (req, res) => {
+    res.render("home")
+})
+
+app.get('/cats', (req, res) => {
+    const cats = [
+        'Blue', 'Rock', 'Cheese'
+    ]
+    res.render('cats', { cats })
+})
+
+app.get('/rand', (req, res) => {
+    const num = Math.floor(Math.random()*10) + 1;
+    res.render('random', { num })
+})
+
+app.get('/r/:subreddit', (req, res) => {
+    const { subreddit } = req.params;
+    const data = redditData[subreddit];
+    if (data) {
+        res.render('subreddit', { ...data })
+    }
+    else {
+        res.render('notFound', {subreddit})
+    }
+    
+})
+
+app.listen(3000, () => {
+    console.log("listening on port 3000")
+})
