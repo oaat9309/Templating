adde15e588ef2687b94d5107a7973fe654765c83
oaat9309 <kite1993@naver.com>
2021-12-08 11:37:40 +0900

    (12.08) navbar imported
    - applied Bootstrap
    - navbar, footer, head ejs added

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const app = express();
 const path = require('path');
 const redditData = require('./data.json')
 
+app.use(express.static(path.join(__dirname, "public")))
+
 app.set('view engine', 'ejs')
+//path.join 뒤에 오는건 views 폴더의 절대 경로를 지정한 것으로, js 파일이 실행되는 디렉토리와 상관없이 잘 실해되도록 지정하는것
 app.set('views', path.join(__dirname, "/views"))
 
 app.get("/", (req, res) => {
@@ -19,7 +22,7 @@ app.get('/cats', (req, res) => {
 
 app.get('/rand', (req, res) => {
     const num = Math.floor(Math.random()*10) + 1;
-    res.render('random', { num })
+    res.render('random', { num, title: "random" })
 })
 
 app.get('/r/:subreddit', (req, res) => {
